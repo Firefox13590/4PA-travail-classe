fdf3c191006724db4448e05f8ae85b4355a783bf
Firefox13590 <pharlyb@gmail.com>
2026-03-04 15:21:50 -0500

    fix seo componenet avec un hook custom

--- teetim-react/src/components/Principal.tsx
+++ teetim-react/src/components/Principal.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import ListeProduits from './ListeProduits';
 import './Principal.css';
-import Seo from './SeoComponent';
+import useSeo from '../hooks/useSeo';
 
 
 
@@ -10,57 +10,56 @@ export default function Principal(props: IPropPrincipal) {
     // console.log('etat du filtre: ', filtre);
     const [tri, setTri] = useState<Tri>("aleatoire");
     // console.log('etat du tri: ', tri);
+    useSeo({
+        titre: "teeTIM - teeshirts 100% cotton bio",
+        description: "Nos teeshirts sont faits avec les meilleurs matériaux, fabriqués au Québec, conçus par ..."
+    });
+
 
 
     return (
-        <>
-            <Seo
-                titre="teeTIM - teeshirts 100% cotton bio"
-                description="Nos teeshirts sont faits avec les meilleurs matériaux, fabriqués au Québec, conçus par ..."
-            />
-            <main className='Principal page-produits page-teeshirts'>
-                <article className="amorce">
-                    <h1>Nos teeshirts</h1>
-                    <section className="controle">
-                        <form>
-                            <div className="filtre">
-                                <label htmlFor="filtre">Filtrer par : </label>
-                                <select
-                                    name="filtre"
-                                    id="filtre"
-                                    value={filtre}
-                                    onChange={(e) => setFiltre(e.target.value as Filtre)}
-                                >
-                                    <option value="tous">Tous les produits</option>
-                                    <option value="animaux">Animaux</option>
-                                    <option value="inusite">Inusité</option>
-                                    <option value="nature">Nature</option>
-                                    <option value="sport">Sport</option>
-                                </select>
-                            </div>
-                            <div className="tri">
-                                <label htmlFor="tri">Trier par : </label>
-                                <select
-                                    name="tri"
-                                    id="tri"
-                                    value={tri}
-                                    onChange={(e) => setTri(e.target.value as Tri)}
-                                >
-                                    <option value="aleatoire">Aléatoire</option>
-                                    <option value="prix_ASC">Prix / ascendant</option>
-                                    <option value="prix_DESC">Prix / descendant</option>
-                                    <option value="nom_ASC">Nom / ascendant</option>
-                                    <option value="nom_DESC">Nom / descendant</option>
-                                    <option value="dac_DESC">Nouveauté</option>
-                                    <option value="ventes_DESC">Meilleur vendeur</option>
-                                </select>
-                            </div>
-                        </form>
-                    </section>
-                </article>
+        <main className='Principal page-produits page-teeshirts'>
+            <article className="amorce">
+                <h1>Nos teeshirts</h1>
+                <section className="controle">
+                    <form>
+                        <div className="filtre">
+                            <label htmlFor="filtre">Filtrer par : </label>
+                            <select
+                                name="filtre"
+                                id="filtre"
+                                value={filtre}
+                                onChange={(e) => setFiltre(e.target.value as Filtre)}
+                            >
+                                <option value="tous">Tous les produits</option>
+                                <option value="animaux">Animaux</option>
+                                <option value="inusite">Inusité</option>
+                                <option value="nature">Nature</option>
+                                <option value="sport">Sport</option>
+                            </select>
+                        </div>
+                        <div className="tri">
+                            <label htmlFor="tri">Trier par : </label>
+                            <select
+                                name="tri"
+                                id="tri"
+                                value={tri}
+                                onChange={(e) => setTri(e.target.value as Tri)}
+                            >
+                                <option value="aleatoire">Aléatoire</option>
+                                <option value="prix_ASC">Prix / ascendant</option>
+                                <option value="prix_DESC">Prix / descendant</option>
+                                <option value="nom_ASC">Nom / ascendant</option>
+                                <option value="nom_DESC">Nom / descendant</option>
+                                <option value="dac_DESC">Nouveauté</option>
+                                <option value="ventes_DESC">Meilleur vendeur</option>
+                            </select>
+                        </div>
+                    </form>
+                </section>
+            </article>
 
-                <ListeProduits {...props} filtre={filtre} tri={tri} />
-            </main>
-        </>
+            <ListeProduits {...props} filtre={filtre} tri={tri} />
+        </main>
     )
 }

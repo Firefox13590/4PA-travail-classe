5c1858faeb8c3922a199623712a89c1dda8c42c0
Firefox13590 <pharlyb@gmail.com>
2026-03-04 15:39:19 -0500

    readying for gh pages

--- teetim-react/vite.config.ts
+++ teetim-react/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/4PA-travail-classe/",
   server: {
     open: true
   }
